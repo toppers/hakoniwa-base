ad3bf0306c33d48e72d899d86ea5d07005d51b6e
森崇 <kanetugu2015@gmail.com>
2022-12-29 18:39:37 +0900

    bug fixed

--- workspace/runtime/install.bash
+++ workspace/runtime/install.bash
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-if [ -f hakoniwa-core-cpp-client/cmake-build/src/hakoc/libhakoc.a  ]
+if [ -f /usr/local/bin/hako-master-rust  ]
 then
-    return;
+    :
+else
+    CURR_DIR=`pwd`
+    cd hakoniwa-core-cpp-client
+    bash build.bash
+
+
+    cd ${CURR_DIR}
+    cd hakoniwa-master-rust/main
+    cp ../../hakoniwa-core-cpp-client/cmake-build/src/hakoc/libshakoc.* /usr/local/lib/hakoniwa/
+    cp ../../hakoniwa-core-cpp-client/cmake-build/src/proxy/hako-proxy /usr/local/bin/hakoniwa/
+
+    bash install.bash
 fi
 
-CURR_DIR=`pwd`
-cd hakoniwa-core-cpp-client
-bash build.bash
-
-
-cd ${CURR_DIR}
-cd hakoniwa-master-rust/main
-cp ../../hakoniwa-core-cpp-client/cmake-build/src/hakoc/libshakoc.* /usr/local/lib/hakoniwa/
-
-bash install.bash
