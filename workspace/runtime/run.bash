f2b6f587aeb0b07d40bb3c72817247ce88d1322b
Takashi Mori <kanetugu2015@gmail.com>
2023-01-01 07:50:51 +0900

    updated for general

--- workspace/runtime/run.bash
+++ workspace/runtime/run.bash
@@ -6,10 +6,22 @@ then
     bash install.bash
 fi
 
-echo "INFO: START hako-master"
+echo "INFO: ACTIVATING HAKO-MASTER"
 hako-master ${DELTA_MSEC} ${MAX_DELAY_MSEC} ${CORE_IPADDR}:${GRPC_PORT} ${UDP_SRV_PORT} ${UDP_SND_PORT} &  
 
-sleep 1
 
-echo "INFO: START hako-proxy"
-hako-proxy ./params/proxy_config.json
+echo "INFO: ACTIVATING ASSET-PROXY"
+
+for info in `cat asset_def.txt`
+do
+    sleep 1
+    TYPE_NAME=`echo $info | awk -F: '{print $1}'`
+    ID=`echo $info | awk -F: '{print $2}'`
+    ASSET_NAME=${TYPE_NAME}-${ID}
+    echo "INFO: START ${ASSET_NAME}"
+    hako-proxy ./params/${ASSET_NAME}/proxy_config.json &
+done
+
+echo "INFO: SIMULATION READY!"
+
+tail -f /dev/null
