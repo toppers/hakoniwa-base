82c8a308d9a365cc0cc72ade958c3f7f139105dc
Takashi Mori <kanetugu2015@gmail.com>
2023-01-28 05:50:24 +0900

    add docker template

---
+++ docker/template/make_dockerfile.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then
+    echo "Usage: $0 <runtime|dev> <app_type>"
+    echo "# <app_type> runtime:"
+    ls docker/template/runtime
+    echo "# <app_type> dev:"
+    ls docker/template/dev
+    exit 1
+fi
+
+TYPE=${1}
+APP_TYPE=${2}
+
+if [ -d docker/template/${TYPE}/${APP_TYPE} ]
+then
+    :
+else
+    echo "ERROR: not found Dockerfile on docker/template/${TYPE}/${APP_TYPE}"
+    exit 1
+fi
+
+cp docker/template/${TYPE}/${APP_TYPE}/${TYPE}_latest_version.txt docker/appendix/
+cp docker/template/${TYPE}/${APP_TYPE}/image_name.txt docker/docker_${TYPE}/
+cp docker/template/${TYPE}/${APP_TYPE}/Dockerfile docker/docker_${TYPE}/
+
+exit 0
