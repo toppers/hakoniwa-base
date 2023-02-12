e40093ebda7f9f06a661e28849753f77f33f3c10
Mitsutaka Takada <mtakada@nces.i.nagoya-u.ac.jp>
2023-02-12 15:32:09 +0900

    Update install.bash for apple silicon target

--- native/template/runtime/ai/mac/install.bash
+++ native/template/runtime/ai/mac/install.bash
@@ -13,31 +13,42 @@ if [ -d ${HAKO_LIBDIR} ]
 then
     :
 else
-    mkdir ${HAKO_LIBDIR}
+sudo    mkdir ${HAKO_LIBDIR}
 fi
 
 if [ -d ${HAKO_BINDIR} ]
 then
     :
 else
-    mkdir ${HAKO_BINDIR}
+sudo    mkdir ${HAKO_BINDIR}
 fi
 
 #install libshakoc.dylib
+if [ $1 = arm ]
+then
+LIBNAME=libshakoc.arm64.dylib
+wget https://github.com/toppers/hakoniwa-core-cpp-client/releases/download/v1.0.1/${LIBNAME}
+else
 LIBNAME=libshakoc.dylib
 wget https://github.com/toppers/hakoniwa-core-cpp-client/releases/download/v1.0.0/${LIBNAME}
+fi
 sudo cp ${LIBNAME} ${HAKO_LIBDIR}/hakoc.so
 sudo mv ${LIBNAME} ${HAKO_LIBDIR}/libshakoc.dylib
 
 #install py libraries
-cp -rp ./hakoniwa-core-cpp-client/py /usr/local/lib/hakoniwa/
+sudo cp -rp ./hakoniwa-core-cpp-client/py /usr/local/lib/hakoniwa/
 
 #install Python3
 brew install python3
 pip3 install numpy
 
 #install hakoniwa-conductor
+if [ $1 = arm ]
+then
+HAKO_CONDUCTOR=hakoniwa-conductor-mac-arm64
+else
 HAKO_CONDUCTOR=hakoniwa-conductor-mac
+fi
 wget https://github.com/toppers/hakoniwa-conductor/releases/download/v1.0.0/${HAKO_CONDUCTOR}
 chmod +x ${HAKO_CONDUCTOR} 
 sudo mv ${HAKO_CONDUCTOR} /usr/local/bin/hakoniwa/hakoniwa-conductor
