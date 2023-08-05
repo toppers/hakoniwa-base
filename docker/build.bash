dd4aa587ab9e0e6f4008cf0895984bde3f70647b
Takashi Mori <kanetugu2015@gmail.com>
2023-08-06 07:38:27 +0900

    bug fixed

--- docker/build.bash
+++ docker/build.bash
@@ -15,31 +15,33 @@ IMAGE_TAG=`cat docker/appendix/dev_latest_version.txt`
 DOCKER_IMAGE=toppersjp/${IMAGE_NAME}:${IMAGE_TAG}
 
 ARCH=`arch`
-
-if [ $OS_TYPE = "Darwin" ]
+OS_TYPE=`bash docker/utils/detect_os_type.bash`
+echo $ARCH
+echo $OS_TYPE
+if [ $OS_TYPE = "Mac" ]
 then
-	if [ $ARCH = "arm64" ]
-	then
-		docker run \
-			--platform linux/amd64 \
-			-v ${HOST_WORKDIR}:${DOCKER_DIR} \
-			-it --rm \
-			--net host \
-			-e APP_NAME=${APP_NAME} \
-			--name ${IMAGE_NAME} ${DOCKER_IMAGE} 
-	else
-		docker run \
-			-v ${HOST_WORKDIR}:${DOCKER_DIR} \
-			-it --rm \
-			--net host \
-			-e APP_NAME=${APP_NAME} \
-			--name ${IMAGE_NAME} ${DOCKER_IMAGE} 
-	fi
+    if [ $ARCH = "arm64" ]
+    then
+        docker run \
+            --platform linux/amd64 \
+            -v ${HOST_WORKDIR}:${DOCKER_DIR} \
+            -it --rm \
+            --net host \
+            -e APP_NAME=${APP_NAME} \
+            --name ${IMAGE_NAME} ${DOCKER_IMAGE} 
+    else
+        docker run \
+            -v ${HOST_WORKDIR}:${DOCKER_DIR} \
+            -it --rm \
+            --net host \
+            -e APP_NAME=${APP_NAME} \
+            --name ${IMAGE_NAME} ${DOCKER_IMAGE} 
+    fi
 else
-	docker run \
-		-v ${HOST_WORKDIR}:${DOCKER_DIR} \
-		-it --rm \
-		--net host \
-		-e APP_NAME=${APP_NAME} \
-		--name ${IMAGE_NAME} ${DOCKER_IMAGE} 
+    docker run \
+        -v ${HOST_WORKDIR}:${DOCKER_DIR} \
+        -it --rm \
+        --net host \
+        -e APP_NAME=${APP_NAME} \
+        --name ${IMAGE_NAME} ${DOCKER_IMAGE} 
 fi
