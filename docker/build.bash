0b6f2d762846dacfa38d5dd785bfb8ca6ca1794c
Takashi Mori <kanetugu2015@gmail.com>
2023-08-06 07:24:54 +0900

    buid for apple silicon

--- docker/build.bash
+++ docker/build.bash
@@ -14,9 +14,32 @@ IMAGE_NAME=`cat docker/docker_dev/image_name.txt`
 IMAGE_TAG=`cat docker/appendix/dev_latest_version.txt`
 DOCKER_IMAGE=toppersjp/${IMAGE_NAME}:${IMAGE_TAG}
 
-docker run \
-	-v ${HOST_WORKDIR}:${DOCKER_DIR} \
-	-it --rm \
-	--net host \
-	-e APP_NAME=${APP_NAME} \
-	--name ${IMAGE_NAME} ${DOCKER_IMAGE} 
+ARCH=`arch`
+
+if [ $OS_TYPE = "Darwin" ]
+then
+	if [ $ARCH = "arm64" ]
+	then
+		docker run \
+			--platform linux/amd64 \
+			-v ${HOST_WORKDIR}:${DOCKER_DIR} \
+			-it --rm \
+			--net host \
+			-e APP_NAME=${APP_NAME} \
+			--name ${IMAGE_NAME} ${DOCKER_IMAGE} 
+	else
+		docker run \
+			-v ${HOST_WORKDIR}:${DOCKER_DIR} \
+			-it --rm \
+			--net host \
+			-e APP_NAME=${APP_NAME} \
+			--name ${IMAGE_NAME} ${DOCKER_IMAGE} 
+	fi
+else
+	docker run \
+		-v ${HOST_WORKDIR}:${DOCKER_DIR} \
+		-it --rm \
+		--net host \
+		-e APP_NAME=${APP_NAME} \
+		--name ${IMAGE_NAME} ${DOCKER_IMAGE} 
+fi
